f1ab6ff0a3ad9f988534dd0117d4b198e55e6736
bjaashish <aashishb89@gmail.com>
2023-09-08 00:00:00 +0530

    profile UI added

--- frontend/src/pages/Profile.js
+++ frontend/src/pages/Profile.js
@@ -1,19 +1,25 @@
 import React from 'react';
+import './styles/Profile.css'; // Import your CSS file
 
 const Profile = () => {
-  // Extract user data from context or API
-  const userData = {
-    username: 'john_doe',
-    email: 'john@example.com',
-    // ... add more user data
-  };
-
   return (
-    <div className="profile-page">
-      <h2>Profile</h2>
-      <p>Username: {userData.username}</p>
-      <p>Email: {userData.email}</p>
-      {/* ... additional profile information */}
+    <div className="profile-container">
+      <div className="profile-picture">
+        <img src="https://cdn.pixabay.com/photo/2015/06/22/08/40/child-817373_1280.jpg" />
+      </div>
+      <div className="profile-info">
+        <h1 className="profile-name">Shrey</h1>
+        <div className="profile-stats">
+          <div className="followers">
+            <span className="count">500</span>
+            <span className="label">Followers</span>
+          </div>
+          <div className="following">
+            <span className="count">200</span>
+            <span className="label">Following</span>
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
